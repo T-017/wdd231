c3cd7d7e6c038c98559058f6bf6af2066e80395d
T-017 <jholt2@byupathway.edu>
2026-07-04 16:40:20 -0500

    header and script

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,18 @@
-document.getElementById("currentYear").innerHTML = document.currentYear = new Date().getFullYear();
-document.getElementById("lastModified").innerHTML = document.lastModified;
-
 const menuButton = document.getElementById("menuButton");
 const navLinks = document.getElementById("navLinks");
 
 menuButton.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-
-  if (navLinks.classList.contains("open")) {
-    menuButton.textContent = "☰";
-  } else {
-    menuButton.textContent = "✖";
-  }
+  navLinks.classList.toggle("active");
+  menuButton.textContent = navLinks.classList.contains("active") ? "✖" : "☰";
 });
+
+const filterButtons = document.querySelectorAll(".filter-button");
+const coursesList = document.querySelectorAll(".courses-list");
+
+filterButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    
+})
+
+document.getElementById("currentYear").innerHTML = document.currentYear = new Date().getFullYear();
+document.getElementById("lastModified").innerHTML = document.lastModified;
