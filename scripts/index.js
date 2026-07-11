d60b91bb78d0d3f3c5b8fc7462279d6c5d1bda23
T-017 <jholt2@byupathway.edu>
2026-07-10 19:23:22 -0500

    edits again

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,3 @@
-const menuButton = document.getElementById("menuButton");
-const navLinks = document.getElementById("navLinks");
-
-menuButton.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-  menuButton.textContent = navLinks.classList.contains("active") ? "✖" : "☰";
-});
-
-const filterButtons = document.querySelectorAll(".filter-button");
-const coursesList = document.querySelectorAll(".courses-list");
-
-filterButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    filterButtons.forEach(button => button.classList.remove("active"));
-    button.classList.add("active");
-
-    const filter = button.getAttribute("data-filter");
-
-    coursesList.forEach(list => {
-      if (filter === "all") {
-        list.style.display = "block";
-      } else {
-        if (list.getAttribute("data-category") === filter) {
-          list.style.display = "block";
-        } else {
-          list.style.display = "none";
-        }
-      }
-    });
-  });
-});
-
-filterButtons.forEach(button => {
-  button.setAttribute("aria-label", `Filter courses by ${button.textContent}`);
-})
-
 const courses = [
   {
     subject: "CSE",
@@ -130,7 +94,6 @@ function filterCourses(filterType) {
       button.setAttribute("aria-pressed", "true");
     }
   });
-
 }
   
 function initializePage() {
@@ -192,6 +155,3 @@ function initializePage() {
 }
 
 window.onload = initializePage;
-
-// document.getElementById("currentYear").innerHTML = document.currentYear = new Date().getFullYear();
-// document.getElementById("lastModified").innerHTML = document.lastModified;
