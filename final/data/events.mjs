99fa5e4772535fff8c2754106d7aa611fd601551
T-017 <jholt2@byupathway.edu>
2026-08-10 16:38:58 -0500

    events update

--- final/data/events.mjs
+++ final/data/events.mjs
@@ -1,51 +1,51 @@
 const places = [
   {
-    name: "Park",
-    address: "City of Anna, TX",
-    description: "Anna has a number of parks and recreational areas that offer a variety of outdoor activities for residents and visitors. Enjoy walking trails, playgrounds, and picnic areas in our beautiful green spaces.",
-    image: "https://www.vmcdn.ca/f/files/localprofile/images/2023_08_document-4-scaled-e1693322909469.jpeg;w=960"
+    name: "Goryōkaku Fort",
+    address: "44-44 Goryokakucho, Hakodate, Hokkaido 040-0001, Japan",
+    description: "Goryōkaku Fort is a star-shaped fort in Hakodate, Hokkaido, Japan. It is famous for its unique design and historical significance, offering visitors a glimpse into Japan's Edo period and beautiful surrounding gardens. During the cherry blossom season, the fort becomes a popular spot for hanami (flower viewing) and photography.",
+    image: "https://lh3.googleusercontent.com/gps-cs-s/AHRPTWlAhjmDXqAG4cUA3nlsXwvvyoTwASVPC4zdVsnnhWsUaxOS8BNT3OJCHLw9hjsN761qPz4OBUgMmH5HoU3e5SgwNcen59G4DbyYYGL2m7Y71fV9R3emDfRPKi4WEop1msVSFqNMn-5iWtbM=s680-w680-h510-rw"
   },
   {
-    name: "Park 1",
-    address: "City of Anna, TX",
-    description: "Anna Park 1 offers a serene environment for relaxation and outdoor activities. It features walking trails, open green spaces, and playgrounds for children.",
-    image: "https://www.vmcdn.ca/f/files/localprofile/images/2023_08_document-4-scaled-e1693322909469.jpeg;w=960"
+    name: "Otaru Snow Light Path Festival",
+    address: "Otaru, Hokkaido, Japan",
+    description: "The Otaru Snow Light Path Festival is a magical winter event held in Otaru, Hokkaido, Japan. The city is adorned with beautiful snow and illuminated lanterns, creating a picturesque and romantic atmosphere. Visitors can enjoy walking along the canals, viewing the intricate snow sculptures, and experiencing the charm of Otaru during the winter season.",
+    image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/d8/29/e3/aviary-image-1581244459908.jpg?w=1000&h=600&s=1"
   },
   {
-    name: "Park 3",
-    address: "City of Anna, TX",
-    description: "Anna Park 3 is a popular spot for families and outdoor enthusiasts. It features sports fields, picnic areas, and scenic walking paths.",
-    image: "https://www.vmcdn.ca/f/files/localprofile/images/2023_08_document-4-scaled-e1693322909469.jpeg;w=960"
+    name: "Sapporo Snow Festival",
+    address: "Odori Park, Sapporo, Hokkaido, Japan",
+    description: "The Sapporo Snow Festival is one of Japan's largest winter events, held annually in Sapporo, Hokkaido. The festival features spectacular snow and ice sculptures, attracting visitors from around the world. Odori Park becomes a winter wonderland with beautifully illuminated sculptures and various entertainment options.",
+    image: "https://hokkaido.a4jp.com/wp-content/uploads/2021/09/sapporo-snow-festival-thumbnail.jpg"
   },
   {
-    name: "Park 4",
-    address: "City of Anna, TX",
-    description: "Anna Park 4 offers a peaceful retreat with beautiful gardens, walking paths, and recreational facilities for all ages.",
-    image: "https://www.vmcdn.ca/f/files/localprofile/images/2023_08_document-4-scaled-e1693322909469.jpeg;w=960"
+    name: "Asahikawa Summer Festival",
+    address: "Asahikawa, Hokkaido, Japan",
+    description: "The Asahikawa Summer Festival is a lively event held in Asahikawa, Hokkaido, Japan. The festival features traditional dances, music performances, and vibrant parades, attracting both locals and tourists. Visitors can enjoy the festive atmosphere, local food stalls, and cultural activities throughout the city.",
+    image: "https://kawatabi-hokkaido.com/wp-content/uploads/2026/07/%E6%97%AD%E5%B7%9D%E5%A4%8F%E3%81%BE%E3%81%A4%E3%82%8A%E3%83%BB%E9%81%93%E6%96%B0%E7%B4%8D%E6%B6%BC%E8%8A%B1%E7%81%AB%E5%A4%A7%E4%BC%9A.webp"
   },
   {
-    name: "Park 5",
-    address: "City of Anna, TX",
-    description: "Anna Park 5 is a vibrant community space with sports facilities, playgrounds, and picnic areas for families and friends to enjoy.",
-    image: "https://www.vmcdn.ca/f/files/localprofile/images/2023_08_document-4-scaled-e1693322909469.jpeg;w=960"
+    name: "Lake Shikotsu Ice Festival",
+    address: "Lake Shikotsu, Hokkaido, Japan",
+    description: "The Lake Shikotsu Ice Festival is a winter event held at Lake Shikotsu in Hokkaido, Japan. The festival features stunning ice sculptures, illuminated displays, and various winter activities for visitors to enjoy.",
+    image: "https://hokkaidoguide.com/wp-content/uploads/2024/02/untitled-76-scaled.jpg"
   },
   {
-    name: "Park 6",
-    address: "City of Anna, TX",
-    description: "Anna Park 6 is a scenic park located on the southern edge of the city. It offers walking trails, picnic areas, and recreational facilities for all ages.",
-    image: "https://www.vmcdn.ca/f/files/localprofile/images/2023_08_document-4-scaled-e1693322909469.jpeg;w=960"
+    name: "Noboribetsu Jigoku Matsuri",
+    address: "Noboribetsu, Hokkaido, Japan",
+    description: "The Noboribetsu Jigoku Matsuri is a vibrant festival held in Noboribetsu, Hokkaido, Japan. The festival celebrates the region's volcanic activity with various events, performances, and cultural displays, attracting both locals and tourists.",
+    image: "https://cdn.gaijinpot.com/app/uploads/sites/6/2024/08/pixta_42472572_M-1024x640.jpg"
   },
   {
-    name: "Park 7",
-    address: "City of Anna, TX",
-    description: "Anna Park 7 is a charming park located in the northern part of the city. It features walking trails, playgrounds, and picnic areas for visitors to enjoy.",
-    image: "https://www.vmcdn.ca/f/files/localprofile/images/2023_08_document-4-scaled-e1693322909469.jpeg;w=960"
+    name: "Wakkanai Minato Antarctic Festival",
+    address: "Wakkanai, Hokkaido, Japan",
+    description: "The Wakkanai Minato Antarctic Festival is an annual event held in Wakkanai, Hokkaido, Japan. The festival celebrates the city's connection to Antarctic exploration and features various cultural performances, exhibitions, and activities for visitors of all ages.",
+    image: "https://www.kyuhoshi.com/wp-content/uploads/2016/07/wakkanai_summer_festival.jpg"
   },
   {
-    name: "Park 8",
-    address: "City of Anna, TX",
-    description: "Anna Park 8 is a lively park that hosts community events, outdoor concerts, and cultural festivals. It features open green spaces, walking trails, and recreational facilities for all ages.",
-    image: "https://www.vmcdn.ca/f/files/localprofile/images/2023_08_document-4-scaled-e1693322909469.jpeg;w=960"
+    name: "Kushiro Port Festival",
+    address: "Kushiro, Hokkaido, Japan",
+    description: "Kushiro Port is a bustling port area in Kushiro, Hokkaido, Japan. It hosts various events, including seafood markets, cultural festivals, and maritime activities. Visitors can enjoy fresh seafood, scenic views, and the lively atmosphere of the port.",
+    image: "https://kawatabi-hokkaido.com/wp-content/uploads/2026/07/%E3%81%8F%E3%81%97%E3%82%8D%E6%B8%AF%E3%81%BE%E3%81%A4%E3%82%8A_%E3%82%A2%E3%82%A4%E3%82%AD%E3%83%A3%E3%83%83%E3%83%81-1.webp"
   }
 ];
 
